571e7e5b31061b6578656f758d23098a85aaee35
qriwty <arthur.social.rech@gmail.com>
2017-09-10 19:18:06 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.open').click(function(e) {
+    $('.menu').addClass('active');
+    $('.menu__bar').addClass('active');
+    $('.global').addClass('menu-active');
+});
+$('.close').click(function(e) {
+    $('.menu').removeClass('active');
+    $('.menu__bar').removeClass('active');
+    $('.global').removeClass('menu-active');
+});
+$('.menu').click(function(e) {
+    if ($(e.target).closest('.menu__bar').length == 0) {
+        $('.menu').removeClass('active');
+        $('.menu__bar').removeClass('active');
+        $('.global').removeClass('menu-active');
+    }
+});
